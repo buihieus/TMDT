58de22fd37fe2f654445032d4444ecbe235867e1
Phungdat2305 <phungdat230503@gmail.com>
2024-12-16 09:35:20 +0700

    Update product.model.js

--- models/product.model.js
+++ models/product.model.js
@@ -1,53 +1,34 @@
 const mongoose = require("mongoose");
-// const slug = require("mongoose-slug-updater");
-// mongoose.plugin(slug);
+const slug = require('mongoose-slug-updater');
 
-const productSchema = new mongoose.Schema(
-  {
-    title: String,
-    product_category_id: {
-      type: String,
-      default: "",
-    },
-    description: String,
-    price: Number,
-    discountPercentage: Number,
-    stock: Number,
-    thumbnail: String,
-    status: String,
-    featured: String,
-    position: Number,
-    slug: {
-      type: String,
-      slug: "title",
-      unique: true,
-    },
-    createdBy: {
-      account_id: String,
-      createdAt: {
-        type: Date,
-        default: Date.now,
-      },
-    },
-    deleted: {
-      type: Boolean,
-      default: false,
-    },
-    deletedBy: {
-      account_id: String,
-      deletedAt: Date,
-    },
-    updatedBy: [
-      {
-        account_id: String,
-        updatedAt: Date,
-      },
-    ],
+mongoose.plugin(slug)
+
+const productSchema = new mongoose.Schema({
+  title: String,
+  category_id: {
+    type: String,
+    default: ""
   },
-  {
-    timestamps: true,
-  }
-);
+  description: String,
+  price: Number,
+  discountPercentage: Number,
+  stock: Number,
+  thumbnail: String,
+  status: String,
+  position: Number,
+  slug: { 
+    type: String, 
+    slug: "title",
+    unique: true 
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  deletedAt: Date,  
+}, {
+  timestamps: true
+});
 
 const Product = mongoose.model("Product", productSchema, "products");
 
